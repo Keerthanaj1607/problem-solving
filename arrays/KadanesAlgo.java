8ff664edb9ac1d9819f76113fcf242cbea96efd2
Keerthana Jayaram <keerthanajayaram77@gmail.com>
2026-03-18 21:38:27 +0530

    Kadanes Algo solved

---
+++ arrays/KadanesAlgo.java
@@ -0,0 +1,22 @@
+public class KadanesAlgo{
+    //function to find maximum subarray
+    public int maxSubArray(int[]nums){
+        int max= Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>max){
+                max=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+       int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };  
+       KadanesAlgo k1= new KadanesAlgo();
+       System.out.println(k1.maxSubArray(arr));
+    }
+}
